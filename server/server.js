663ff4799815e11437529cd2d32078c549b13760
David Galindo <david.galindo.dgp@gmail.com>
2026-04-13 13:48:37 +0200

    first commit

---
+++ server/server.js
@@ -0,0 +1,43 @@
+//import express 
+import express from 'express';
+import path from 'path';
+//import connectToDB
+import { connectToDB } from './db.js';
+
+import { fileURLToPath } from 'url';
+
+
+
+const app = express();
+const PORT = process.env.PORT || 1337;
+
+//json and cookies
+app.use(express.json());
+
+console.log(process.env.MONGO_URI);
+
+// __dirname route
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// __dirname route to frontend
+const frontendPath = path.join(__dirname, '../frontend');
+
+// static
+app.use(express.static(frontendPath));
+
+// routes
+app.get('/', (req, res) => {
+    res.sendFile(path.join(frontendPath, 'index.html'));
+});
+
+// Page 404
+app.use((req, res) => {
+  res.status(404).sendFile('404.html', { root: frontendPath });
+});
+// connect Mongo
+connectToDB();
+// start server
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`, `http://localhost:${PORT}`);
+});
