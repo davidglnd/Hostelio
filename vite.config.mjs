88a9e162896ad2ef75c771358d41bdb0b4208102
David Galindo <david.galindo.dgp@gmail.com>
2026-05-15 13:30:09 +0200

    chore(deploy) update Vite config

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,6 +1,18 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        login: resolve(__dirname, 'frontend/pages/login.html'),
+        signup: resolve(__dirname, 'frontend/pages/signup.html'),
+        dashboard: resolve(__dirname, 'frontend/pages/dashboard.html'),
+        expenses: resolve(__dirname, 'frontend/pages/expenses.html'),
+      }
+    }
+  },
   server: {
     proxy: {
       '/api': {
