b6bf542db452920dd6312613a0187b64b76e6047
David Galindo <david.galindo.dgp@gmail.com>
2026-09-06 13:02:56 +0200

    fix:

--- frontend/js/classes/User.js
+++ frontend/js/classes/User.js
@@ -1,5 +1,7 @@
-export class User{
-    constructor (firstName, lastName, email, password, role = "basic", businessName) {
+export class User {
+    #password;
+
+    constructor(firstName, lastName, email, password, role = "basic", businessName) {
         this.firstName = firstName.trim().toLowerCase();
         this.lastName = lastName.trim().toLowerCase();
         this.email = email.trim().toLowerCase();
